d8c53e80a6d1afdf067f6626b82efb4034569407
cyxo33dy <meeran@st.ovgu.de>
2023-06-05 00:31:25 +0200

    added api calls to infocard

--- social-media-frontend/src/components/ProfileInfo.jsx
+++ social-media-frontend/src/components/ProfileInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function ProfileInfo() {
@@ -8,29 +8,57 @@ function ProfileInfo() {
     const [work, setWork] = useState('-');
     const [rel, setRel] = useState('-');
     const [edit, setEdit] = useState(true);
+    const [bio, setBio] = useState(null)
+
     const triggerEdit = () => {
         return setEdit( !edit )
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const info = {rel, loc, work};
-
-        console.log(info)
+        setEdit( !edit )
+        const bio = {rel, loc, work};
+        
+        fetch('http://localhost:3000/bio/1', {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({
+                rel: rel,
+                work: work,
+                loc: loc,
+                id: 1
+            })
+        })
+        .then(res => res.json())
     }
+
+    useEffect(() => {
+        fetch('http://localhost:3000/bio')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBio(data);
+            })
+    },[bio])
 
   return (
     <form onSubmit={handleSubmit}>
+        { bio &&
     <div className='flex flex-col gap-3 bg-slate-200 p-4 rounded-lg w-90% shadow-md mt-3 ml-3 font-poppins'>
         <div className="flex justify-between items-center cursor-pointer">
             <h4 className='text-lg'><b>Bio</b></h4>
         </div>
+        {bio.map(bios => ( 
+        <div className="bio" key={bios.id}>
         <div className="info">
             <span>
                 <b>Relationship Status<br></br></b>
             </span>
             {edit ? (
-            <span>{rel}</span>
+            <span>{bios.rel}</span>
             ) : (
                 <select
                     value={rel}
@@ -48,7 +76,7 @@ function ProfileInfo() {
                 <b>Lives in<br></br></b>
             </span>
             {edit ? (
-            <span>{loc}</span>
+            <span>{bios.loc}</span>
             ) : (
              <input type='text' onChange={(e) => setLoc(e.target.value)}></input>
             )}
@@ -58,22 +86,25 @@ function ProfileInfo() {
                 <b>Works at<br></br></b>
             </span>
             {edit ? (
-            <span>{work}</span>
+            <span>{bios.work}</span>
             ) : (
                 <input type='text' onChange={(e) => setWork(e.target.value)}></input>
                 )}
         </div>
+        </div>
+        ))}
         <div className='self-end'>
         {edit ? (
         <button onClick={triggerEdit} 
         class=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</button>
         ) : (
-            <button onClick={triggerEdit} 
+            <button onClick={handleSubmit} 
             type="button"
             class=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">Done</button>   
         )}
         </div>
     </div>
+}
     </form>
   )
 }
