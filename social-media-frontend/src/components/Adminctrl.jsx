febba2992f405dd05487598298e04a87c5843801
abhishreya <abhishreya.sharma@ovgu.de>
2023-06-20 18:46:38 +0200

    admin feed improved

--- social-media-frontend/src/components/Adminctrl.jsx
+++ social-media-frontend/src/components/Adminctrl.jsx
@@ -20,11 +20,11 @@ const Adminctrl = (props) => {
     event.preventDefault();
     setEditingUser(user.user_id);
     setEditedUserFields({
-      user_name: user.user_name,
-      email: user.email,
-      date_of_birth: user.date_of_birth,
-      country: user.country,
-      city: user.city,
+      user_name: user.user_name || '',
+      email: user.email || '',
+      date_of_birth: user.date_of_birth || '',
+      country: user.country || '',
+      city: user.city || '',
       lives_in: user.lives_in || 'NA',
       relationship_status: user.relationship_status || 'NA',
       works_at: user.works_at || 'NA'
@@ -57,72 +57,109 @@ const Adminctrl = (props) => {
       <div className="grid grid-cols-4 gap-4">
         {users.map(user => (
           <div key={user.user_id} className="border p-4 rounded-lg shadow-md">
-            {editingUser === user.user_id ? (
-              <div>
+            <div className="grid grid-cols-1">
+              <div className="mb-2">
+                <label className="font-bold">User Name:</label>
                 <input
                   type="text"
-                  value={editedUserFields.user_name || ''}
+                  value={editingUser === user.user_id ? editedUserFields.user_name : user.user_name}
                   onChange={event => handleFieldChange('user_name', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Email:</label>
                 <input
                   type="text"
-                  value={editedUserFields.email || ''}
+                  value={editingUser === user.user_id ? editedUserFields.email : user.email}
                   onChange={event => handleFieldChange('email', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Date of Birth:</label>
                 <input
                   type="text"
-                  value={editedUserFields.date_of_birth || ''}
+                  value={editingUser === user.user_id ? editedUserFields.date_of_birth : user.date_of_birth}
                   onChange={event => handleFieldChange('date_of_birth', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Country:</label>
                 <input
                   type="text"
-                  value={editedUserFields.country || ''}
+                  value={editingUser === user.user_id ? editedUserFields.country : user.country}
                   onChange={event => handleFieldChange('country', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">City:</label>
                 <input
                   type="text"
-                  value={editedUserFields.city || ''}
+                  value={editingUser === user.user_id ? editedUserFields.city : user.city}
                   onChange={event => handleFieldChange('city', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Lives In:</label>
                 <input
                   type="text"
-                  value={editedUserFields.lives_in || ''}
+                  value={editingUser === user.user_id ? editedUserFields.lives_in : user.lives_in || 'NA'}
                   onChange={event => handleFieldChange('lives_in', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Relationship Status:</label>
                 <input
                   type="text"
-                  value={editedUserFields.relationship_status || ''}
+                  value={editingUser === user.user_id ? editedUserFields.relationship_status : user.relationship_status || 'NA'}
                   onChange={event => handleFieldChange('relationship_status', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
+              </div>
+              <div className="mb-2">
+                <label className="font-bold">Works At:</label>
                 <input
                   type="text"
-                  value={editedUserFields.works_at || ''}
+                  value={editingUser === user.user_id ? editedUserFields.works_at : user.works_at || 'NA'}
                   onChange={event => handleFieldChange('works_at', event.target.value)}
+                  readOnly={editingUser !== user.user_id}
+                  className={`border rounded p-1 ${editingUser === user.user_id ? '' : 'border-transparent'}`}
                 />
-                <button onClick={() => handleSaveUser(user)}>Save</button>
               </div>
-            ) : (
-              <div>
-                <h3 className="text-lg font-bold mb-2">
-                <a href={`/user/${user.user_id}`} className="text-blue-500 hover:underline">
-                {user.user_name}
-              </a>
-                </h3>
-                <p className="mb-2">Email: {user.email}</p>
-                <p className="mb-2">Date of Birth: {user.date_of_birth}</p>
-                <p className="mb-2">Country: {user.country}</p>
-                <p className="mb-2">City: {user.city}</p>
-                <p className="mb-2">Lives In: {user.lives_in || 'NA'}</p>
-                <p className="mb-2">Relationship Status: {user.relationship_status || 'NA'}</p>
-                <p className="mb-2">Works At: {user.works_at || 'NA'}</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" onClick={event => handleEditUser(event, user)}>Edit</button>
-              </div>
-            )}
+              {editingUser === user.user_id ? (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
+                  onClick={() => handleSaveUser(user)}
+                >
+                  Save
+                </button>
+              ) : (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
+                  onClick={event => handleEditUser(event, user)}
+                >
+                  Edit
+                </button>
+              )}
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+  
+};
 
 export default Adminctrl;
