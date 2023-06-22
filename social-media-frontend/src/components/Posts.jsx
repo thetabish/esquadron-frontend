947f77a092d48deb2e0c311016da3ac1cde60b58
cyxo33dy <meeran@st.ovgu.de>
2023-06-22 23:26:27 +0200

    the user and their friend's posts are only visible

--- social-media-frontend/src/components/Posts.jsx
+++ social-media-frontend/src/components/Posts.jsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import Post from './Post.jsx'
+import Post from './Post.jsx';
 
 const Posts = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const currentUserId = userData.id;
 
-    useEffect(() => {
-      fetchPosts();
+  useEffect(() => {
+    fetchPosts();
 
-      const interval = setInterval(fetchPosts, 5000);
+    const interval = setInterval(fetchPosts, 5000);
 
-      return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:5000/get-posts');
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/get-posts/${currentUserId}`);
+      if (response.ok) {
         const data = await response.json();
         setPosts(data.posts);
-      } catch (error) {
-        console.error('Error fetching posts:', error);
+      } else {
+        throw new Error('Request failed with status ' + response.status);
       }
-    };
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
 
-    
-    return (
-      <div>
-          {posts.map((post, id)=>{
-              return <Post data={post} id={id}/>
-          })}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {posts.map((post, id) => {
+        return <Post data={post} id={id} key={id} />;
+      })}
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
