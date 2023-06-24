cdac52f41b39aa3d966da143732734bf4c17b8c4
cyxo33dy <meeran@st.ovgu.de>
2023-06-24 18:08:22 +0200

    Posts count

--- social-media-frontend/src/components/MainProfileCard.jsx
+++ social-media-frontend/src/components/MainProfileCard.jsx
@@ -2,30 +2,30 @@ import React, { useRef, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Cover from "../assets/cover.jpg";
 import Profile from "../assets/profileImg.jpg";
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 
 const MainProfileCard = () => {
   const { viewedProfileId } = useParams();
   const fileInputRef = useRef(null);
-  const userData = JSON.parse(localStorage.getItem('userData'));
+  const userData = JSON.parse(localStorage.getItem("userData"));
   const isCurrentUser = userData.id == viewedProfileId;
   const [following, setFollowing] = useState([]);
   const [followers, setFollowers] = useState([]);
-
+  const [postCount, setPostCount] = useState(0);
   const handleEditProfilePicture = (event) => {
     const file = event.target.files[0]; // Get the selected file from the file input
-  
+
     // Check if a file is selected
     if (file) {
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('user_id', userData.id); // Pass the user ID to the backend
-  
-      fetch('http://127.0.0.1:5000/upload-profile-picture', {
-        method: 'POST',
+      formData.append("file", file);
+      formData.append("user_id", userData.id); // Pass the user ID to the backend
+
+      fetch("http://127.0.0.1:5000/upload-profile-picture", {
+        method: "POST",
         body: formData,
       })
-      .then((response) => response.text())
+        .then((response) => response.text())
         .then((filename) => {
           console.log(filename); // Handle the response from the backend
 
@@ -39,46 +39,64 @@ const MainProfileCard = () => {
         .then((response) => response.blob())
         .then((blob) => {
           // Create object URL from the blob
-          if(blob.type != "text/html"){
+          if (blob.type != "text/html") {
             const url = URL.createObjectURL(blob);
-            setProfilePictureUrl(url);  
+            setProfilePictureUrl(url);
           }
-  
+
           // Update the profile picture URL in the userData object
           const updatedUserData = { ...userData, profilePicture: url };
-          localStorage.setItem('userData', JSON.stringify(updatedUserData));
-  
+          localStorage.setItem("userData", JSON.stringify(updatedUserData));
+
           // Update the profile picture URL in the UI
           setProfilePictureUrl(url);
         })
         .catch((error) => {
-          console.error('Error:', error);
+          console.error("Error:", error);
         });
     }
   };
-  
+
   const [profilePictureUrl, setProfilePictureUrl] = useState(null);
   const imageRef = useRef();
 
-  useEffect(() => {
-    // Fetch the profile picture URL from the backend
-    fetch(`http://127.0.0.1:5000/profile-picture/${viewedProfileId}`)
-      .then(response => response.blob())
-      .then(blob => {
-        if(blob.type != "text/html"){
-          const url = URL.createObjectURL(blob);
-          setProfilePictureUrl(url);  
+  useEffect(
+    () => {
+      // Fetch the user's posts count
+      const fetchPostCount = async () => {
+        try {
+          const userData = JSON.parse(localStorage.getItem("userData"));
+          const currentUserId = userData.id;
+          const response = await fetch(
+            `http://127.0.0.1:5000/get-user-posts/${currentUserId}`
+          );
+          const data = await response.json();
+          const { posts } = data;
+          const count = posts.length; // Number of posts
+          setPostCount(count);
+        } catch (error) {
+          console.error("Error fetching post count:", error);
         }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        // Set a default profile picture URL or handle the error as needed
-      });
+      };
+      fetchPostCount();
+      // Fetch the profile picture URL from the backend
+      fetch(`http://127.0.0.1:5000/profile-picture/${viewedProfileId}`)
+        .then((response) => response.blob())
+        .then((blob) => {
+          if (blob.type != "text/html") {
+            const url = URL.createObjectURL(blob);
+            setProfilePictureUrl(url);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          // Set a default profile picture URL or handle the error as needed
+        });
       // Make the API call to get the list of friends for the current user
-      fetch('http://127.0.0.1:5000/get-friends', {
-        method: 'POST',
+      fetch("http://127.0.0.1:5000/get-friends", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ user_id: userData.id }),
       })
@@ -87,13 +105,13 @@ const MainProfileCard = () => {
           setFollowing(friendsData);
         })
         .catch((error) => {
-          console.error('Error fetching friends:', error);
+          console.error("Error fetching friends:", error);
         });
 
-        fetch('http://127.0.0.1:5000/get-followers', {
-        method: 'POST',
+      fetch("http://127.0.0.1:5000/get-followers", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ user_id: userData.id }),
       })
@@ -102,9 +120,13 @@ const MainProfileCard = () => {
           setFollowers(friendsData);
         })
         .catch((error) => {
-          console.error('Error fetching friends:', error);
+          console.error("Error fetching friends:", error);
         });
-  }, [viewedProfileId], [userData.id], [userData.id]);
+    },
+    [viewedProfileId],
+    [userData.id],
+    [userData.id]
+  );
 
   const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
@@ -120,18 +142,18 @@ const MainProfileCard = () => {
           <img src={Cover} alt="" className="w-full h-80 object-cover" />
         </div>
         {profilePictureUrl ? (
-  <img
-    src={profilePictureUrl}
-    alt=""
-    className="w-40 h-40 rounded-full absolute bottom-[-3rem] shadow-lg shadow-indigo-500/50"
-  />
-) : (
-  <img
-    src={Profile}
-    alt=""
-    className="w-40 h-40 rounded-full absolute bottom-[-3rem] shadow-lg shadow-indigo-500/50"
-  />
-)}
+          <img
+            src={profilePictureUrl}
+            alt=""
+            className="w-40 h-40 rounded-full absolute bottom-[-3rem] shadow-lg shadow-indigo-500/50"
+          />
+        ) : (
+          <img
+            src={Profile}
+            alt=""
+            className="w-40 h-40 rounded-full absolute bottom-[-3rem] shadow-lg shadow-indigo-500/50"
+          />
+        )}
       </div>
       <div className="ProfileName flex flex-col items-center justify-center mt-12 gap-2">
         <span className="font-bold text-xl">{userData.user_name}</span>
@@ -147,7 +169,7 @@ const MainProfileCard = () => {
           <span>Followers</span>
         </div>
         <div className="follow flex flex-col gap-1 items-center justify-center">
-          <span className="font-bold">3</span>
+          <span className="font-bold">{postCount}</span>
           <span>Posts</span>
         </div>
       </div>
